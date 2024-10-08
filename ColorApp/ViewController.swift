905b90cdf7f947f9b24e98d39d5bd6962e583b69
Varya Utkina <varya2412@mail.ru>
2024-10-08 17:54:02 +0300

    ViewController: work with logic

--- ColorApp/ViewController.swift
+++ ColorApp/ViewController.swift
@@ -8,12 +8,53 @@
 import UIKit
 
 final class ViewController: UIViewController {
-
+    // MARK: - IB Outlets
+    @IBOutlet var coloredView: UIView!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    // MARK: - View Life Cycles
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
     }
-
-
+    
+    // MARK: - Override Methods
+    
+    // MARK: - IB Actions
+    
+    @IBAction func redSliderAction() {
+        redLabel.text = String(format: "%.2f", redSlider.value)
+        setupUI()
+    }
+    
+    @IBAction func greenSliderAction() {
+        greenLabel.text = String(format: "%.2f", greenSlider.value)
+        setupUI()
+    }
+    @IBAction func blueSliderAction() {
+        blueLabel.text = String(format: "%.2f", blueSlider.value)
+        setupUI()
+    }
+    
+    // MARK: - Private Methods
+    private func setupUI() {
+        redLabel.text = String(format: "%.2f", redSlider.value)
+        greenLabel.text = String(format: "%.2f", greenSlider.value)
+        blueLabel.text = String(format: "%.2f", blueSlider.value)
+        
+        coloredView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
 }
 
